6120bc8728352e3af8b15bfbe7ed46e809d9c4db
crs <Chris_deFontaine@outlook.com>
2026-03-18 17:48:04 +0800

    add RawFileReaderS as first option

--- SCMeTA/file/thermo.py
+++ SCMeTA/file/thermo.py
@@ -1,9 +1,5 @@
 import pandas as pd
-from scipy.signal import find_peaks
-
 from .format import SCData
-from RawFileReader.src.RawFileReader import RawFileReader
-
 
 def load_txt(path):
     raw = pd.read_table(path)
@@ -12,16 +8,47 @@ def load_txt(path):
     return raw
 
 def load_thermo(path, load_range, include_ms2=False, filter_resolution: bool = True) -> pd.DataFrame:
-    reader = RawFileReader(file_path=path)
-    data = reader.to_dataframe(include_ms2=include_ms2)
-    if filter_resolution:
-        peaks, _ = find_peaks(data["Intensity"], distance=16)
-        data = data.iloc[peaks]
-    data.set_index("Scan", inplace=True)
-    data = data.loc[load_range[0]:load_range[1]] if load_range else data
-    data = data.drop(columns=["RetentionTime"])
 
-    return data
+    try:
+        import RawFileReaderS
+        from tqdm import trange        
+        import numpy as np
+        import os
+
+        raw = RawFileReaderS.RawFile(path)
+
+        file_name = os.path.splitext(os.path.basename(path))[0]
+        scan_nums, masses, intensities = [], [], []
+        if load_range is None:
+            load_range = (1, raw.n_scans)
+        for i in trange(load_range[0], load_range[1] + 1, desc=f"Parsing {file_name}"):
+                mzs, ints = raw.scan(i)
+                
+                data_points_count = len(mzs)
+                if data_points_count > 0:
+                    scan_nums.append(np.full(data_points_count, i))
+                    masses.append(mzs)
+                    intensities.append(ints)
+
+        final_df = pd.DataFrame({
+            "Mass": np.concatenate(masses),
+            "Intensity": np.concatenate(intensities),
+            "Scan": np.concatenate(scan_nums)
+        })
+    except :
+        print("Failed to load with RawFileReaderS, trying rolling back to RawFileReader...")
+        from RawFileReader.src.RawFileReader import RawFileReader
+        from scipy.signal import find_peaks
+        reader = RawFileReader(file_path=path)
+        data = reader.to_dataframe(include_ms2=include_ms2)
+        if filter_resolution:
+            peaks, _ = find_peaks(data["Intensity"], distance=16)
+            data = data.iloc[peaks]
+        data.set_index("Scan", inplace=True)
+        data = data.loc[load_range[0]:load_range[1]] if load_range else data
+        final_df = data.drop(columns=["RetentionTime"])
+
+    return final_df
 
 def load_thermo_data(name, path, target_attr, load_range, include_ms2=False, filter_resolution: bool = True) -> SCData:
     df = load_thermo(path=path, load_range=load_range, include_ms2=include_ms2, filter_resolution=filter_resolution)
